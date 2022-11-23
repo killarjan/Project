e7314d6a5935314a23ddd54b349fc2f56ab1fe85
killarjan <70253855+killarjan@users.noreply.github.com>
2022-11-24 00:02:24 +0700

    Настройка мигратора

--- src/UserManagementSystem.Migrator/Program.cs
+++ src/UserManagementSystem.Migrator/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program 
+{ 
+    static void Main(string[] args) 
+    {
+        RunMigrator.WithParameters(DbKind.Postgres, "Host=localhost:5432;Database=UserManagementSystem;Username=postgres;Password=mysecretpassword", typeof(Program).Assembly, args) ; 
+    } 
+}
