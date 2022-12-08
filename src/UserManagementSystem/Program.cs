c7b1daab7a9abe17107f8bd28e589e78797d5802
killarjan <70253855+killarjan@users.noreply.github.com>
2022-12-08 22:59:13 +0700

    Создана основа Web API

--- src/UserManagementSystem/Program.cs
+++ src/UserManagementSystem/Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.AspNetCore.Hosting;
+
 namespace UserManagementSystem
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
