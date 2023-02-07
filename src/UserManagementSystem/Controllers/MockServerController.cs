3b6ae125e08ec41f5eb762f766bd5d2db0a1897b
killarjan <70253855+killarjan@users.noreply.github.com>
2023-02-07 22:03:51 +0700

    Добавлен контроллер обращения к Mock серверу

---
+++ src/UserManagementSystem/Controllers/MockServerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using UserManagementSystem.Models.Users.Responses;
+
+namespace UserManagementSystem.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MockServerController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory = null;
+
+        public MockServerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Call()
+        {            
+            using (HttpClient client = _httpClientFactory.CreateClient())
+            {
+                MockServerResponce? mockServerResponce = await client.GetFromJsonAsync<MockServerResponce>("https://3vw153md8d.api.quickmocker.com/test");
+                return Ok(mockServerResponce);
+            }
+        }
+    }
+}   
